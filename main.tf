76da0103dfd70b14a15cffa29b870819790744f1
s.raidu <s.raidu@hotmail.com>
2022-03-29 12:53:36 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create AWS ec2 instance
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  security_groups= [var.security_group]
+  }
