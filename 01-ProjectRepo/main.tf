19a01a4014009817fd3fb6e192d127dc0706ae74
s.raidu <s.raidu@hotmail.com>
2022-04-18 22:53:19 +0800

    Terraform Projects Sync to GitHub

--- 01-ProjectRepo/main.tf
+++ 01-ProjectRepo/main.tf
@@ -9,3 +9,8 @@ resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   }
+
+output "instance_ip_addr" {
+  value = "aws_instance.myFirstInstance.private_ip"
+  description = "The private IP address of the main server instance."
+}
